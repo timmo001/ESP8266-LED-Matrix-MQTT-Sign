2a2cab3463181c3ecb47f2c48dc61d58199d4c34
Aidan <28114703+timmo001@users.noreply.github.com>
2018-05-14 20:39:29 +0100

    Formatting

--- src/setup-template.h
+++ src/setup-template.h
@@ -12,9 +12,9 @@ using namespace std;
 #define mqtt_extras_topic "display/matrix001/extras"
 #define mqtt_extras_set_topic "display/matrix001/extras/set"
 
-const char *ssid = "ssid";        // SSID of local network
-const char *password = "pass";       // Password on network
-const char *ota_password = "pass";    // OTA Password
+const char *ssid = "ssid";                // SSID of local network
+const char *password = "pass";            // Password on network
+const char *ota_password = "pass";        // OTA Password
 const char *ota_hostname = "max7219_001"; // OTA Hostname
 
 const char *mqtt_name = "matrix_clock_001";
@@ -24,8 +24,8 @@ const char *mqtt_user = "user";
 const char *mqtt_password = "pass";
 
 const string hass_host = "domain_or_ip"; // HASS Hostname
-const uint16_t hass_port = 443;                  // HASS Port. Use Port 80 for http and 443 for https
-const string hass_api_password = "pass";     // HASS Password
+const uint16_t hass_port = 443;          // HASS Port. Use Port 80 for http and 443 for https
+const string hass_api_password = "pass"; // HASS Password
 
 const long utcOffset = 1;
 
